ca5892f99212bcf52d2f18bbf17c6af78e4eb568
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-21 11:05:19 +0900

    M[

--- js-form.js
+++ js-form.js
@@ -16,6 +16,7 @@ function handleSubmit(event) {
     const currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);
+    console.log(input.value);
 }
 
 function askForName () {
